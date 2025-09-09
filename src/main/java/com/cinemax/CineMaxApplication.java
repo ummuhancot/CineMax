9cbbafae5859a314c51b36225e22e36169d94127
ummuhanı çot <ummuhancot@gmail.com>
2025-09-09 15:24:08 +0300

    CineMax first commit

---
+++ src/main/java/com/cinemax/CineMaxApplication.java
@@ -0,0 +1,13 @@
+package com.cinemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMaxApplication.class, args);
+	}
+
+}
