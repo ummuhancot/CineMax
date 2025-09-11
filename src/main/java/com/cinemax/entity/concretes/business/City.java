7a22c3d26c7dcba1abf7bc95afb010b2bdaa4eb1
guney <guneyblks.java@gmail.com>
2025-09-11 21:01:46 +0300

    CityEntity_belkisguney

--- src/main/java/com/cinemax/entity/concretes/business/City.java
+++ src/main/java/com/cinemax/entity/concretes/business/City.java
@@ -1,5 +1,42 @@
 package com.cinemax.entity.concretes.business;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cities")
 public class City {
-    //belkis güney
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotNull
+    @Size(max = 30, message = "City name cannot be longer than 30 characters")
+    private String name;
+
+    //@JoinColumn(name = "country_id")
+    //private Country country;
+
+
+    /**
+     CITY
+     Field	Type	Properties
+     userId	Long(PK)	Auto increment
+     name	String	Not null, max: 30
+     countryId	Long(FK)
+
+     */
 }
