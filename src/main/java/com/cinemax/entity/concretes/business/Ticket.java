ec6c6b1583623d07ff3b18ad77123da5ab0f3752
ummuhanı çot <ummuhancot@gmail.com>
2025-09-11 22:10:04 +0300

    Ticket ve TicketStatus updated

--- src/main/java/com/cinemax/entity/concretes/business/Ticket.java
+++ src/main/java/com/cinemax/entity/concretes/business/Ticket.java
@@ -1,5 +1,52 @@
 package com.cinemax.entity.concretes.business;
 
+import com.cinemax.entity.enums.TicketStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "ticket")
 public class Ticket {
-    //ummuhanı çot
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String seatLetter;
+
+    @Column(nullable = false)
+    private String seatNumber;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TicketStatus ticketStatus = TicketStatus.RESERVED;
+
+    //@Enumerated(EnumType.STRING)
+    //private PaymentStatus paymentStatus;
+
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd MMM yyyy, HH:mm")
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd MMM yyyy, HH:mm")
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
