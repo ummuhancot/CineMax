6e0ec7f4a5616e31d2fd87cfa85b669d0f4702f6
kemalergorun <kemalergorun@gmail.com>
2025-09-11 15:38:26 +0300

    kemal-showtime class

--- src/main/java/com/cinemax/entity/concretes/business/ShowTime.java
+++ src/main/java/com/cinemax/entity/concretes/business/ShowTime.java
@@ -1,5 +1,44 @@
 package com.cinemax.entity.concretes.business;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "showtimes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShowTime {
-    //kemal ergorun
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hall_id")
+    private Hall hall;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
