2fcd6f1270266c7e0f9b0d8b12750c27c685e175
ummuhanı çot <ummuhancot@gmail.com>
2025-09-30 21:47:49 +0300

    Revert "Get<< /api/movies?... (it will return movies based on query and paging parameters)(sorgu ve sayfalama parametrelerine göre filmler döndürecektir. /api/movies/coming-soon (it will return movies that are not in theaters but coming soon based on the search date)(Arama tarihine göre sinemalarda olmayan ancak yakında vizyona girecek filmleri döndürecektir)"
    This reverts commit c15c15f1

--- src/main/java/com/cinemax/repository/businnes/MovieRepository.java
+++ src/main/java/com/cinemax/repository/businnes/MovieRepository.java
@@ -5,34 +5,22 @@ import com.cinemax.entity.enums.MovieStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
 @Repository
-public interface MovieRepository extends
-        JpaRepository<Movie, Long>,
-        JpaSpecificationExecutor<Movie> { // Specification + paging/sorting
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     boolean existsBySlug(String slug);
-
     Page<Movie> findByHalls_Name(String hallName, Pageable pageable);
-
-    // Vizyonda olanlar (status)
+    // 1) Sadece status ile (vizyonda olanlar)
     Page<Movie> findByStatus(MovieStatus status, Pageable pageable);
 
-    // Vizyonda olup gösterim tarihi başlamış olanlar (status + tarih, "in-theaters/active")
+    // 2) Hem status hem tarih ile (vizyonda olup gösterim tarihi başlamış olanlar)
     Page<Movie> findByStatusAndReleaseDateBefore(
             MovieStatus status,
             LocalDate today,
-            Pageable pageable
-    );
-
-    // T-5: Yakında vizyona girecekler (status + ileri tarih)
-    Page<Movie> findByStatusAndReleaseDateAfter(
-            MovieStatus status,
-            LocalDate after,
             Pageable pageable
     );
 }
