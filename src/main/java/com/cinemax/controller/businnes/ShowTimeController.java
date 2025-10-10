dd2625e8dcde169f50594049bb5278836b8b6db5
ummuhanı çot <ummuhancot@gmail.com>
2025-10-10 10:37:58 +0300

    getShowTimeById showtime nin bilgileri ayrıntılı görüntülenecek

--- src/main/java/com/cinemax/controller/businnes/ShowTimeController.java
+++ src/main/java/com/cinemax/controller/businnes/ShowTimeController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/showtime")
@@ -23,6 +20,12 @@ public class ShowTimeController {
     @PreAuthorize("hasAnyAuthority('Admin','Manager','Customer')")
     public ResponseEntity<ShowTimeResponse> createShowTime(@Valid @RequestBody ShowTimeRequest request) {
         ShowTimeResponse response = showTimeService.createShowTime(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ShowTimeResponse> getShowTimeById(@PathVariable Long id) {
+        ShowTimeResponse response = showTimeService.getShowTimeById(id);
         return ResponseEntity.ok(response);
     }
 }
